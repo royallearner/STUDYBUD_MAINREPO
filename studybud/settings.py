29c5230401775a7ef7e67e5c150816d29912314a
Royal <royalalearner@gmail.com>
2025-12-24 09:34:02 +0530

    Fixed datset url

--- studybud/settings.py
+++ studybud/settings.py
@@ -104,7 +104,8 @@ DATABASES = {
     'default': dj_database_url.config(
         # Replace this value with your local database's connection string.
         default=os.environ.get('DATABASE_URL'),
-        conn_max_age=600
+        conn_max_age=600,
+        ssl_require=True
     )
 }
 
