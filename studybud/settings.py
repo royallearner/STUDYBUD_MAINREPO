0a640e5681ce73b25e7154493eb21fcb05c33c3a
Royal <royalalearner@gmail.com>
2025-12-24 09:00:38 +0530

    cleared errors

--- studybud/settings.py
+++ studybud/settings.py
@@ -13,6 +13,8 @@ import dj_database_url
 import os
 
 from pathlib import Path
+from django.core.management.utils import get_random_secret_key
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
