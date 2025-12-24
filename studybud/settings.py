e7734850de750a78386b5a01bb78e5c695f8fbdb
Royal <royalalearner@gmail.com>
2025-12-24 10:42:28 +0530

    Static configuration checked(4th time)

--- studybud/settings.py
+++ studybud/settings.py
@@ -174,7 +174,7 @@ STATIC_URL = '/static/'
 if not DEBUG:
     STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
-    STATICFILES_STORAGE = 'whitenoise.storage.ManifestStaticFilesStorage'
+    STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
 
 DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
 
